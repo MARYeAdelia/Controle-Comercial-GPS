334c81f03c8a0521e1904c0c87928685e2a7583a
MARYeAdelia <72323269+MARYeAdelia@users.noreply.github.com>
2026-08-25 20:44:40 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -592,9 +592,9 @@ function SecaoGerencial({ data, setData }) {
               <div style={{overflowX:"auto"}}>
                 <table style={{width:"100%",borderCollapse:"collapse",tableLayout:"fixed"}}>
                   <colgroup>
-                    <col style={{width:80}}/><col style={{width:"auto"}}/><col style={{width:60}}/>
-                    <col style={{width:80}}/><col style={{width:65}}/><col style={{width:65}}/>
-                    <col style={{width:80}}/><col style={{width:110}}/><col style={{width:110}}/>
+                    <col style={{width:70}}/><col style={{width:220}}/><col style={{width:55}}/>
+                    <col style={{width:75}}/><col style={{width:60}}/><col style={{width:60}}/>
+                    <col style={{width:75}}/><col style={{width:105}}/><col style={{width:105}}/>
                   </colgroup>
                   <thead>
                     <tr style={{background:"#F8FAFC"}}>
